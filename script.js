6860b2c0e0b3c1cd337a60e87ca2c459b3232311
flaviovitale02 <flaviovitale73@gmail.com>
2023-05-15 17:00:07 +0200

    modifica

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const inputKm =parseInt( document.getElementById (userKm))
-const inputAge = parseInt(document.getElementById (userAge)) 
+const inputKm = document.getElementById (userKm)
+const inputAge =   document.getElementById (userAge)
 const button = document.querySelector ('button.button')
 const priceKm = 0.233
 let price
 
 button.addEventListener('click', function(){
-    price = inputKm * priceKm
-    if(inputAge < 18){
+    price = inputKm.value * priceKm
+    if(inputAge.value < 18){
         price = price - price(100 / 19.4)
     }
-    else if(inputAge > 65){
+    else if(inputAge.value > 65){
         price = price - price(100 / 37.7)
     }
     console.log (price)
