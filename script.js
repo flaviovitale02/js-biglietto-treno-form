894f8941e36e0f6dee7fc85fae0402257aa4fb20
flaviovitale02 <flaviovitale73@gmail.com>
2023-05-15 18:12:31 +0200

    MILESTONE 1

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-const inputKm = document.getElementById (userKm)
-const inputAge =   document.getElementById (userAge)
+const inputKm = document.getElementById ('userKm')
+const inputAge =   document.getElementById ('userAge')
 const button = document.querySelector ('button.button')
 const priceKm = 0.233
 let price
+let result
 
 button.addEventListener('click', function(){
     price = inputKm.value * priceKm
     if(inputAge.value < 18){
-        price = price - price(100 / 19.4)
+        price = price - (price * 19.5 / 100)
     }
-    else if(inputAge.value > 65){
-        price = price - price(100 / 37.7)
+    else if(inputAge.value >= 65){
+        price = price - (price * 37.7 /100)
     }
     console.log (price)
+    document.getElementById('result').innerHTML = price.toFixed(2)
 })
